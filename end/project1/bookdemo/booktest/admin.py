ffde75756c312868e5a0531fc75829da52b917ea
wh0371 <377588092@qq.com>
2020-02-11 17:56:10 +0800

    测试数据 创建超级用户 创建后台管理

--- end/project1/bookdemo/booktest/admin.py
+++ end/project1/bookdemo/booktest/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 # Django自带的后台管理操作需要在此实现
+
+from .models import Book, Hero
+admin.site.register(Book)
+admin.site.register(Hero)
--- end/project1/bookdemo/booktest/models.py
+++ end/project1/bookdemo/booktest/models.py
@@ -23,6 +23,9 @@ class Book(models.Model):
     price = models.FloatField(default=0)
     pub_date = models.DateField(default="1983-06-01")
 
+    def __str__(self):
+        return self.title
+
 
 class Hero(models.Model):
     """
@@ -33,3 +36,11 @@ class Hero(models.Model):
     content = models.CharField(max_length=100)
     # book  是一对多中的外键  on_delete代表删除主表数据时数据如何做
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# django  orm关联查询
+# 多方Hero  一方Book
+# 1.多找一  多方对象.关系字段  exp:h1.book
+# 2.一找多  一方对象,小写多放类名_set.all()  exp:b1.hero_set.all()
