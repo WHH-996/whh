fc5c3ab61c0f4ef5475abbc4981416aa0847b70c
wh0371 <377588092@qq.com>
2020-02-14 14:01:44 +0800

    模板文件与静态文件

--- end/project1/bookdemo/bookdemo/urls.py
+++ end/project1/bookdemo/bookdemo/urls.py
@@ -19,15 +19,26 @@ from django.urls import path, include
 # 路由 网址 每一个网址均需要绑定视图函数  视图函数给予页面返回
 # MVT V视图函数 3个作用 接收请求 处理数据 返回响应
 
-
+# 可以实现一个路由分流
 urlpatterns = [
     path('admin/', admin.site.urls),
     # 1. 使用path 将booktest的路由 进行包含
-    path('booktest/', include('booktest.urls'))
+    # path('booktest/', include('booktest.urls'))
+    path('', include('booktest.urls', namespace='booktest'))
 
 ]
 
 # 项目的所有路由地址配置文件
 # admin路由是Django自带的后台管理路由
 
-# 总的路由匹配文件
+# 总的路由匹配文件 == 项目路由文件  使用include包含应用路由文件
+# 硬编码  在html文件中有很多超级链接 其中href属性 如果写成绝对路径  这种就叫硬编码
+# 在开发的过程中  可能需要反复修改路由 若使用硬编码不方便
+# 需要解除硬编码
+# 1.需要给应用一个 app_name = "应用名" 写在应用的urls.py
+# 2.在项目路由中给应用分流时  在include中 提供命名空间
+# 3.在应用中给每一个路由一个名字
+# 4.在html中使用时 href = "{% url '命名空间名:路由name' 实参列表 %}"
+
+# 以前定位路由 靠总路由正则表达+应用路由正则表达式
+# 解除
--- end/project1/bookdemo/booktest/views.py
+++ end/project1/bookdemo/booktest/views.py
@@ -4,7 +4,7 @@ from .models import Book, Hero
 # Create your views here.
 
 # MVT 中的V 视图模块
-# 在此处请求接收 处理数据 返回响应
+# 在此处接收请求 处理数据 返回响应
 
 # 3.编写对应的视图函数
 from django.http import HttpResponse
